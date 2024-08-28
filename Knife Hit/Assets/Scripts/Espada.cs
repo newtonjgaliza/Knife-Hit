7da4a25bbb0afaa8a88250ca1de4f00d6df7904b
newtonjgaliza <newtonjgaliza@gmail.com>
2024-08-28 20:47:49 -0300

    Update Espada.cs
    aula 4

--- Knife Hit/Assets/Scripts/Espada.cs
+++ Knife Hit/Assets/Scripts/Espada.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Espada : MonoBehaviour
 {
+    [Header("Referências Gerais")]
     private Rigidbody2D oRigidbody2D;
 
+    [Header("Lançamento da Espada")]
     [SerializeField] private float forcaDoLancamento = 20f;
     private bool foiLancada = false;
 
@@ -35,4 +37,26 @@ public class Espada : MonoBehaviour
         oRigidbody2D.AddForce(new Vector2(0f, forcaDoLancamento), ForceMode2D.Impulse);
         oRigidbody2D.gravityScale = 1f;
     }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(!foiLancada)
+        {
+            if(collider.gameObject.GetComponent<Alvo>() != null)
+            {
+                oRigidbody2D.velocity = Vector2.zero; //new Vector2(0, 0)
+                oRigidbody2D.gravityScale = 0f;
+                oRigidbody2D.bodyType = RigidbodyType2D.Static;
+                transform.SetParent(collider.gameObject.transform);
+            }
+            else if(collider.gameObject.GetComponent<Espada>() != null)
+            {
+                oRigidbody2D.velocity = Vector2.zero;
+            }
+
+            foiLancada = true;
+        }
+    }
+
+
 }
